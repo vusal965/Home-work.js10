6dd3e48c37e0b264faa5111befbeb416ecf8e92f
vusal965 <vusalbaxsiyev756@gmail.com>
2024-07-12 18:28:49 +0400

    Home work

---
+++ app.js
@@ -0,0 +1,25 @@
+let num1 = 50;
+
+
+while (num1 <= 1000) {
+   
+    console.log(num1);
+    num1 += 100;
+}
+
+let num2 = 11;
+
+while (num2 % 101) {
+    
+    console.log(num2);
+    num2 += 2;
+   
+}
+
+// let num3 = +prpmpt(" Eded daxil edin:")
+
+// while (num3 % 1) {
+//     let num4 = num3 % 100
+// }
+
+
